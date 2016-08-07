c4a0dfdf010fb0a742f3bd18c9e7477b9022a411
kampeepan <botaney_babe@hotmail.com>
2016-08-07 11:26:03 +0700

    Json String

--- app/src/main/java/egat/kampeepan/kampeepanservice/MainActivity.java
+++ app/src/main/java/egat/kampeepan/kampeepanservice/MainActivity.java
@@ -1,16 +1,24 @@
 package egat.kampeepan.kampeepanservice;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     //Explicit
     private EditText userEditText, passwordEditText;
     private String userString, passwordString;
+    private static final String urlJson = "http://swiftcodingthai.com/6aug/get_user_bow.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,25 +26,66 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Bind Widget
-
         userEditText = (EditText) findViewById(R.id.editText4);
         passwordEditText = (EditText) findViewById(R.id.editText5);
 
-
     }   //Main Method
+
+    //Create Inner Class
+    private class SynchronizeUser extends AsyncTask<Void, Void, String> {
+
+        //Explicit
+        private Context context;    //โยน data ระหว่าง class ต้องต่อท่อ context
+        private String myUserString, myPasswordString;  //รับค่า string จาก main class
+
+        public SynchronizeUser(Context context, String myUserString, String myPasswordString) {
+            this.context = context;
+            this.myUserString = myUserString;
+            this.myPasswordString = myPasswordString;
+        }   //Alt+Insert -> Constructor
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            try {
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urlJson).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+                //Success
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+                //Unsuccess
+            }
+
+            //return null;
+        }   //doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.d("7AugV1", "JSON ==> " + s);
+        }   //onPost
+    }   //Sync User Class
+
 
     public void clickSignIn(View view){
         userString = userEditText.getText().toString().trim();
         passwordString = passwordEditText.getText().toString().trim();
 
+        //Check Space
         if (userString.equals("") || passwordString.equals("")) {
             MyAlert myAlert = new MyAlert();
             myAlert.myDialog(this, 3, "Have Space", "Please Complete your Username and Password");
         } else {
-
+            //Call Sync User
+            SynchronizeUser synchronizeUser = new SynchronizeUser(this, userString, passwordString);
+            synchronizeUser.execute();
         }
 
-    }
+    } //Click Sign In
 
     public void clickSignUpMain(View view) {
 
